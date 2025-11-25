f598730049d94eadd0a1421f60cf4ea71f4f32dd
jhonvincenthernandez <jhonvincenthernandez9@gamil.com>
2025-11-25 10:51:54 +0800

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '77ef68d820935c98fbd9cc10c8d9ba1b785c2abd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '77ef68d820935c98fbd9cc10c8d9ba1b785c2abd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
